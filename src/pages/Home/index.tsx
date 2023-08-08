3c46f5b2681b5e13cfe51b9e45203f7176300fbb
julio cesar <jcso2@aluno.ifal.edu.br>
2023-08-08 01:59:37 -0300

    new changes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,15 @@
+import './styles.css'
+
 export const HomePage = ()=>{
     return(
-        <div className="HomePage">
-            <h1>homePage</h1>
+        <div className="homePage">
+            <div className='title'>
+            <img src="https://assets.stickpng.com/images/5b4a216ec051e602a568cd7e.png" alt="" />
+            <h1>Animais do ifal</h1>
+            </div>
+            <div className='container'>
+                <div className='littleContainer'></div>
+            </div>
         </div>
     )
 }
